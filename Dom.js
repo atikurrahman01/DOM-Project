7189ba4afe9eea3b110ef7ab130b221a93a86af3
Atikurr0201 <mdatikurr950@gmail.com>
2023-11-09 21:31:26 +0600

    2nd dom project end

--- Dom.js
+++ Dom.js
@@ -13,7 +13,7 @@ function subscribe() {
 function calculateTotal() {
 
     const inputElement = document.querySelector('.js-cost-input');
-    let cost = inputElement.value;
+    let cost = Number(inputElement.value);
 
 
     if (cost < 40) {
@@ -23,3 +23,44 @@ function calculateTotal() {
 
     document.querySelector('.js-total-cost').innerHTML = '$' + { cost };
 }
+
+
+
+//2nd project
+
+
+var myName = document.querySelector('#myname');
+
+var btn = document.querySelector('#add');
+
+check = 0
+
+btn.addEventListener('click', function () {
+
+    if (check === 0) {
+        myName.style.color = 'Red';
+        // myName.innerHTML = 'Kashem Mia';
+        check = 1;
+        btn.innerHTML = "Remove";
+        btn.style.backgroundColor = 'red';
+
+
+    }
+    else {
+
+        myName.style.color = 'black';
+        btn.style.backgroundColor = 'black';
+        btn.innerHTML = 'Add me';
+        check = 0;
+    }
+
+
+});
+
+
+
+
+
+
+
+
